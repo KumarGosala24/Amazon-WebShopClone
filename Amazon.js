404133d960a1d3fe3929b7f1daaf3ce3c0a8b931
Kumar Gosala <kumargosala24@gmail.com>
2025-01-02 22:30:36 +0530

    Update Amazon.js

--- Amazon.js
+++ Amazon.js
@@ -1,26 +1,37 @@
-let currentSlide = 0;
 
-function showSlide(index) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
 
-    if (index >= totalSlides) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = totalSlides - 1;
-    } else {
-        currentSlide = index;
+let currentSlideIndex = 0;
+
+function navigateSlide(direction) {
+    const track = document.querySelector('.carousel-track');
+    const totalItems = document.querySelectorAll('.carousel-item').length;
+    currentSlideIndex += direction;
+
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = totalItems - 1; // Loop to the last slide
+    } else if (currentSlideIndex >= totalItems) {
+        currentSlideIndex = 0; // Loop to the first slide
     }
 
-    const offset = -currentSlide * 50;
-    slides.style.transform = `translateX(${offset}%)`;
+    const offset = -currentSlideIndex * 50; // Move by 100% per slide
+    track.style.transform = `translateX(${offset}%)`;
 }
 
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
 
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
 
+let currentSlide = 0;
+
+function navigateSlides(direction) {
+    const track = document.querySelector('.slides');
+    const totalItems = document.querySelectorAll('.slide').length;
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = totalItems - 1; // Loop to the last slide
+    } else if (currentSlide >= totalItems) {
+        currentSlide = 0; // Loop to the first slide
+    }
+
+    const offset = -currentSlide * 100; // Move by 100% per slide
+    track.style.transform = `translateX(${offset}%)`;
+}
